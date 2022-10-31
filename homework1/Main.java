0a78ff5c6d8d6860a8e4e5b63ec701c57afb1a32
skychocowhite <simon2000.wang@gmail.com>
2022-11-01 00:17:01 +0800

    Handle input file

--- homework1/Main.java
+++ homework1/Main.java
@@ -1,7 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    static void main (String[] args) {
+    static BufferedReader inputBuffer;
+
+    static public void main (String[] args) {
         if (args.length != 1) {
-            
+            System.out.println("Input Error");
+            return;
+        }
+
+        try {
+            inputBuffer = new BufferedReader(new FileReader(args[0]));
+        }
+        catch (Exception bufferError) {
+            System.out.println("Input Error");
+            try {
+                inputBuffer.close();
+            }
+            catch (IOException ioError) {
+                
+            }
         }
     }
 }
