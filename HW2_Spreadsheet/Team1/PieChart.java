54ee947960f20f932694d6aa6b70a86f5135fbee
liang <liang@atinfo.com.tw>
2022-11-01 11:36:03 +0800

    摘要：udpate PieChart.java
    原因：
    
    mantis：

--- HW2_Spreadsheet/Team1/PieChart.java
+++ HW2_Spreadsheet/Team1/PieChart.java
@@ -8,7 +8,11 @@ public class PieChart extends Presentation{
     @Override
     public void depict() {
         for(Map.Entry<String, Integer> entry: getSpreadsheetApplication().getData().entrySet()){
-            System.out.println(String.format("%s %.1f%%", entry.getKey(), 100*(float)entry.getValue()/SpreadsheetApplication.totalItemValue));
+            if(SpreadsheetApplication.totalItemValue <= 0){
+                System.out.println(String.format("%s %.1f%%", entry.getKey(), 0));
+            } else{
+                System.out.println(String.format("%s %.1f%%", entry.getKey(), 100*(float)entry.getValue()/SpreadsheetApplication.totalItemValue));
+            }
         }
     }
 }
