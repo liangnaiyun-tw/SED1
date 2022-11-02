edf4aea97d2c489e1ca3ac134c94001eca6e7f1f
liang-nai-yun <ny.liang.tw@gmail.com>
2022-11-02 13:45:35 +0800

    switch case block add default case to print an Input error message

--- HW2_Spreadsheet/Team1/Main.java
+++ HW2_Spreadsheet/Team1/Main.java
@@ -8,24 +8,28 @@ class Main {
         SpreadsheetApplication spreadsheetApplication = new SpreadsheetApplication();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
-            while ((line = reader.readLine()) != null) {
-                String[] keyValuePair = line.split(" ");
-                switch (keyValuePair[0]) {
-                    case "data":
-                        spreadsheetApplication.addData(keyValuePair[1], Integer.parseInt(keyValuePair[2]));
-                        break;
-                    case "addChart":
-                        spreadsheetApplication.addPresentation(keyValuePair[1]);
-                        break;
-                    case "change":
-                        System.out.println(String.format("%s change %s %s.", keyValuePair[1], keyValuePair[2], keyValuePair[3]));
-                        spreadsheetApplication.update(keyValuePair[2], Integer.parseInt(keyValuePair[3]));
-                        break;
-                    default:
-                        System.out.println("Input error");
-                        break;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    String[] keyValuePair = line.split(" ");
+                    switch (keyValuePair[0]) {
+                        case "data":
+                            spreadsheetApplication.addData(keyValuePair[1], Integer.parseInt(keyValuePair[2]));
+                            break;
+                        case "addChart":
+                            spreadsheetApplication.addPresentation(keyValuePair[1]);
+                            break;
+                        case "change":
+                            System.out.println(String.format("%s change %s %s.", keyValuePair[1], keyValuePair[2], keyValuePair[3]));
+                            spreadsheetApplication.update(keyValuePair[2], Integer.parseInt(keyValuePair[3]));
+                            break;
+                        default:
+                            throw new Exception("No command matched");
+                    }
                 }
+            } catch (Exception e) {
+                System.out.println("Input error");
             }
+            
         } catch (IOException e) {
             System.out.println("Input error");
         }
