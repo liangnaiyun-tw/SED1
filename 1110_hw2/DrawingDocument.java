ec5c91c32185bc9400204801278aab2b7a836b2e
shaoyu0966 <b06705028@ntu.edu.tw>
2022-11-05 17:57:24 +0800

    add 11/10 hw2

---
+++ 1110_hw2/DrawingDocument.java
@@ -0,0 +1,5 @@
+public class DrawingDocument implements Document {
+    public void present() {
+        System.out.println("DrawingDocument");
+    }
+}
---
+++ 1110_hw2/TextDocument.java
@@ -0,0 +1,5 @@
+public class TextDocument implements Document {
+    public void present() {
+        System.out.println("TextDocument");
+    }
+}
