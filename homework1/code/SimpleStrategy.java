1ccfc9c3fa73062c5ac2fb6b8876e7daf17cf7df
skychocowhite <simon2000.wang@gmail.com>
2022-11-07 21:24:33 +0800

    Correct test example

--- homework1/code/SimpleStrategy.java
+++ homework1/code/SimpleStrategy.java
@@ -2,7 +2,7 @@ public class SimpleStrategy implements LinebreakingStrategy {
     @Override
     public void arrange(Composition composition) {
         for (Component component: composition.getComponents()) {
-            System.out.println(String.format("[%d]%s", component.getNatrualSize(), component.getContent()));
+            System.out.println(String.format("[%d]%s ", component.getNatrualSize(), component.getContent()));
         }
     }
 }
