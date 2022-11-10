d7329cd8c83b9e3cf51a930738ee10d96928579d
hsujc <hsujc513@gmail.com>
2022-11-10 10:39:35 +0800

    modify Author, BookCopy, Subject

--- midterm/src/Author.java
+++ midterm/src/Author.java
@@ -4,4 +4,12 @@ public class Author {
     public Author(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- midterm/src/BookCopy.java
+++ midterm/src/BookCopy.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookCopy {
     private Author author;
     private Subject subject;
@@ -5,7 +8,52 @@ public class BookCopy {
     private int id;
     private List<Borrower> borrowHistory;
 
-    public BookCopy(String name) {
-        this.name = name;
+    public BookCopy(Author author, Subject subject) {
+        this.author = author;
+        this.subject = subject;
+        this.status = Status.AVAILABLEFORCHECKOUT;
+        this.id = LibrarySystem.bookCopyCount;
+        LibrarySystem.bookCopyCount++;
+        borrowHistory = new ArrayList<Borrower>;
+    }
+
+    public void addBorrowHistory(Borrower borrower){
+        borrowHistory.add(borrower);
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Borrower> getBorrowHistory() {
+        return borrowHistory;
     }
 }
