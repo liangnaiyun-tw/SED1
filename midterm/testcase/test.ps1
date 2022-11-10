8a501476e5961413427f4e518facb8e0d2a5f7da
skychocowhite <simon2000.wang@gmail.com>
2022-11-10 17:36:42 +0800

    Fix testscript

--- midterm/testcase/test.ps1
+++ midterm/testcase/test.ps1
@@ -8,7 +8,7 @@ powershell /c "rm ..$($codeFolder)\*.class"
 Write-Output "Compile java files..."
 powershell /c "javac ..$($codeFolder)\*.java"
 
-$sampleInputs = Get-ChildItem -Name | Select-String -Pattern "sample*.in"
+$sampleInputs = Get-ChildItem -Filter "*.in"
 foreach($sampleInput in $sampleInputs) {
     [string] $sampleInput = $sampleInput
     $length = $sampleInput.length
