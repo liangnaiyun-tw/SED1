4bffd7f38ab9dd5eb334702bcec4aab3215162b1
shaoyu0966 <b06705028@ntu.edu.tw>
2022-11-10 10:41:16 +0800

    add exception, rename to SmallLibrarySystem

--- midterm/src/SmallLibrarySystem.java
+++ midterm/src/SmallLibrarySystem.java
@@ -1,18 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class System {
+public class SmallLibrarySystem {
     private List<BookCopy> bookCopies;
     private List<User> users;
     private List<CheckOut> checkouts;
     public static int bookCopyCount;
 
     // + System(copies: List<BookCopy>, users: List<User>)
-    System(List<BookCopy> copies, List<User> users) {
+    SmallLibrarySystem(List<BookCopy> copies, List<User> users) {
         this.bookCopies = copies;
         this.users = users;
         this.checkouts = new ArrayList<>();
-        System.bookCopyCount = this.bookCopies.size();
+        SmallLibrarySystem.bookCopyCount = this.bookCopies.size();
+    }
+
+    public List<BookCopy> getBookCopies() {
+        return this.bookCopies;
+    }
+
+    public List<User> getUser() {
+        return this.users;
+    }
+
+    public List<CheckOut> getCheckOut() {
+        return this.checkouts;
     }
 
     // + checkOut(bookCopyIds: List<Integer>, staffName: String, borrowerName:
@@ -32,37 +44,39 @@ public class System {
     }
 
     // + removeBook(bookCopyId: int, staffName: String): void
-    public void removeBook(int bookCopyId, String staffName) {
+    public void removeBook(int bookCopyId, String staffName) throws Exception {
 
     }
 
     // + queryByAuthor(author: Author): List<BookCopy>
-    public List<BookCopy> queryByAuthor(Author author) {
-
+    public List<BookCopy> queryByAuthor(Author author) throws Exception {
+        return null;
     }
 
     // + queryBySubject(subject: Subject): List<BookCopy>
-    public List<BookCopy> queryBySubject(Subject subject) {
-
+    public List<BookCopy> queryBySubject(Subject subject) throws Exception {
+        return null;
     }
 
     // + findOutCheckBooks(staffName: String, borrowerName: String): List<BookCopy>
-    public List<BookCopy> findOutCheckBooks(String staffName, String borrowName) {
-
+    public List<BookCopy> findOutCheckBooks(String staffName, String borrowName) throws Exception {
+        return null;
     }
 
     // + findOutLastBorrower(bookCopyId: int, staffName: String): Borrower
-    public Borrower findOutLastBorrower(int bookCopyId, String staffName) {
-
+    public Borrower findOutLastBorrower(int bookCopyId, String staffName) throws Exception {
+        return null;
     }
 
     // - isStaff(userName: String): boolean
     private boolean isStaff(String userName) {
-
+        // TODO
+        return true;
     }
 
     // - isBorrower(userName: String): boolean
     private boolean isBorrower(String userName) {
-
+        // TODO
+        return true;
     }
 };
