1db3603242e0bc05698228e0e544490e9ab79316
skychocowhite <simon2000.wang@gmail.com>
2022-11-10 08:00:30 +0800

    Write Main.java

--- midterm/src/Main.java
+++ midterm/src/Main.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Input Error");
+            return;
+        }
+
+        String inputLine;
+        String[] inputTokens;
+        BufferedReader inputFile;
+        try {
+            inputFile = new BufferedReader(new FileReader(args[0]));
+            while ((inputLine = inputFile.readLine()) != null) {
+                inputTokens = inputLine.split("\\s+");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Input Error");
+        }
     }
 }
