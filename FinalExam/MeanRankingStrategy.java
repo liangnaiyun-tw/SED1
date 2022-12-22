1fe90d29526f5586645b134cf247dd86cd9e4a34
skychocowhite <simon2000.wang@gmail.com>
2022-12-22 14:14:15 +0800

    Mean

--- FinalExam/MeanRankingStrategy.java
+++ FinalExam/MeanRankingStrategy.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,33 +9,31 @@ public class MeanRankingStrategy implements RankingStrategy {
     List<Review> receivedReviews = student.getReceivedReviews();
     double total = 0;
     int cnt = 0;
-    for(Review r:receivedReviews){
-      if(r.getAssignment().getAssignmentId().equals(assignment.getAssignmentId())){
+    for (Review r : receivedReviews) {
+      if (r.getAssignment().getAssignmentId().equals(assignment.getAssignmentId())) {
         List<Criterion> criteria = r.getAssignment().getRubric().getCriteria();
-        for(Criterion c:criteria){
+        for (Criterion c : criteria) {
           total += r.getReviews().get(c).getRate();
           cnt++;
         }
       }
     }
-    return total/cnt;
+    return total / cnt;
   }
 
   @Override
-  public Map<Criterion, Double> calculateScoreGroupByCriterion(
-    Assignment assignment,
-    Student student
-  ) {
+  public Map<Criterion, Double> calculateScoreGroupByCriterion(Assignment assignment,
+      Student student) {
     List<Review> receivedReviews = student.getReceivedReviews();
-    Map<Criterion,Double> ret = new LinkedHashMap<>();
+    Map<Criterion, Double> ret = new LinkedHashMap<>();
     List<Criterion> criteria = assignment.getRubric().getCriteria();
-    for(Criterion c:criteria){
+    for (Criterion c : criteria) {
       ret.put(c, 0.0);
     }
-    for(Review r:receivedReviews){
-      if(r.getAssignment().getAssignmentId().equals(assignment.getAssignmentId())){
-        for(Criterion c:criteria){
-          ret.put(c,ret.get(c)+r.getReviews().get(c).getRate());
+    for (Review r : receivedReviews) {
+      if (r.getAssignment().getAssignmentId().equals(assignment.getAssignmentId())) {
+        for (Criterion c : criteria) {
+          ret.put(c, ret.get(c) + r.getReviews().get(c).getRate());
         }
       }
     }
