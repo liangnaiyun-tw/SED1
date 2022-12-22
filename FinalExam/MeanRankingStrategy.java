ffb3ec2a9d890b7e32aa18662bf4a7ba98b7df24
hsujc <hsujc513@gmail.com>
2022-12-22 14:13:21 +0800

    fix bug

--- FinalExam/MeanRankingStrategy.java
+++ FinalExam/MeanRankingStrategy.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class MeanRankingStrategy implements RankingStrategy {
 
   @Override
-  public double calculateScore(Assignment assignment, Student student) {
+  public double calculateScore(Assignment assignment, Student student){
     List<Review> receivedReviews = student.getReceivedReviews();
     double total = 0;
     int cnt = 0;
@@ -26,20 +26,23 @@ public class MeanRankingStrategy implements RankingStrategy {
   public Map<Criterion, Double> calculateScoreGroupByCriterion(
     Assignment assignment,
     Student student
-  ) {
+  ) throws Exception {
     List<Review> receivedReviews = student.getReceivedReviews();
     Map<Criterion,Double> ret = new LinkedHashMap<>();
     List<Criterion> criteria = assignment.getRubric().getCriteria();
     for(Criterion c:criteria){
       ret.put(c, 0.0);
     }
+    boolean flag = true;
     for(Review r:receivedReviews){
       if(r.getAssignment().getAssignmentId().equals(assignment.getAssignmentId())){
+        if(flag)  flag=false;
         for(Criterion c:criteria){
           ret.put(c,ret.get(c)+r.getReviews().get(c).getRate());
         }
       }
     }
+    if(flag)  throw new Exception("Error");
     return ret;
   }
 }
